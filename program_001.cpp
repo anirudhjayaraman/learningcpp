081fb042b6eb971218cecf9e77d8f3c6e3e06b6b
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-05-08 13:15:23 +0530

    Program 1: Asking user to input favorite number...

---
+++ program_001.cpp
@@ -0,0 +1,14 @@
+/*
+ Ask the user to diplay their favorite number and let them know that their
+ chosen number is your favorite number too!
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  int some_number;
+  std::cout << "What is your favorite number?" << '\n';
+  std::cin >> some_number;
+  std::cout << "Wow, that's my favorite number too!" << '\n';
+  return 0;
+}
