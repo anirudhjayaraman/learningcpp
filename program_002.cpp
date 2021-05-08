5d1bf46e6ec20890ba54cfe2156cc1f2aca0d620
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-05-08 13:25:04 +0530

    Program 2: Example of compiler warning with uninitiated variable

---
+++ program_002.cpp
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  int favorite;
+  std::cout << "/* message */" << '\n';
+  std::cout << "printing favorite number ", favorite << '\n';
+  return 0;
+}
