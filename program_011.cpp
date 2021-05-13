6c1fa466a52e8a500e780ea2171cfe0422008a5b
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-05-13 21:12:27 +0530

    Program 11: sizeof user defined variables added.

--- program_011.cpp
+++ program_011.cpp
@@ -15,15 +15,22 @@ int main(int argc, char const *argv[]) {
   std::cout << sizeof(unsigned long int) << " bytes" << '\n';       // 8
   std::cout << sizeof(short int) << " bytes" << '\n';               // 2
   std::cout << sizeof(unsigned short int) << " bytes" << '\n';      // 2
+  std::cout << "======================" << '\n';
 
   // Use values defined in <climts> to get minimumn values of primitive types
-  std::cout << '\n' << "MINIMUM VALUES OF PRIMITIVE TYPES: " << '\n';
   std::cout << CHAR_MIN << '\n';
   std::cout << INT_MIN << '\n';
   std::cout << SHRT_MIN << '\n';
   std::cout << LONG_MIN << '\n';
   std::cout << LLONG_MIN << '\n';
 
+  std::cout << "======================" << '\n';
+
+  int a {21};
+  double b {444.56};
+
+  std::cout << sizeof a << " bytes" << '\n';
+  std::cout << sizeof(b) << " bytes" << '\n';
   return 0;
 }
 
