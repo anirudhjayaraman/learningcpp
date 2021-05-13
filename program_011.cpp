1ac4141325301dc3a372476756819292be321435
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-05-13 21:07:10 +0530

    Program 11: sizeof primitive types and their minimum values

---
+++ program_011.cpp
@@ -0,0 +1,31 @@
+/*
+sizeof primitive types and their minimum values
+*/
+
+#include <iostream>
+#include <climits>
+
+
+int main(int argc, char const *argv[]) {
+
+  std::cout << sizeof(long long) << " bytes" << '\n';               // 8
+  std::cout << sizeof(long int) <<  " bytes" <<'\n';                // 8
+  std::cout << sizeof(long double) << " bytes" << '\n';             // 8
+  std::cout << sizeof(bool) << " bytes" << '\n';                    // 1
+  std::cout << sizeof(unsigned long int) << " bytes" << '\n';       // 8
+  std::cout << sizeof(short int) << " bytes" << '\n';               // 2
+  std::cout << sizeof(unsigned short int) << " bytes" << '\n';      // 2
+
+  // Use values defined in <climts> to get minimumn values of primitive types
+  std::cout << '\n' << "MINIMUM VALUES OF PRIMITIVE TYPES: " << '\n';
+  std::cout << CHAR_MIN << '\n';
+  std::cout << INT_MIN << '\n';
+  std::cout << SHRT_MIN << '\n';
+  std::cout << LONG_MIN << '\n';
+  std::cout << LLONG_MIN << '\n';
+
+  return 0;
+}
+
+
+// g++ -Wall -std=c++17 program_011.cpp -o output
