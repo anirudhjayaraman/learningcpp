c964c0826a762c6b4346e4a839c330d8c6fc76f7
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-06-27 21:21:44 +0530

    Program 21-22: Coding exercises 7 and 8

---
+++ program_022.cpp
@@ -0,0 +1,42 @@
+/*
+Coding Exercise 8
+*/
+
+#include <iostream>
+using namespace std;
+
+int arithmetic_operators(int number) {
+    int original_number {number};
+    //----WRITE YOUR CODE BELOW THIS LINE----
+
+    //-- Multiply number by 2 and assign the result back to number
+    number = number * 2;
+
+    //-- Add 9 to number and assign the result back to number
+    number = number + 9;
+    //-- Subtract 3 from number and assign the result back to number
+    number = number - 3;
+    //-- Divide number by 2 and assign the result back to number
+    number = number / 2;
+    //-- Subtract original_number from number and assign the result back to number
+    number = number - original_number;
+    //-- Take the modulus 3 (%) of number and assign it back to number
+    number = number % 3;
+
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+
+    return number;
+}
+
+int main(int argc, char const *argv[]) {
+  int number, transform_number;
+  std::cout << "Enter integer number " << '\n';
+  std::cin >> number;
+  transform_number = arithmetic_operators(number);
+  printf("%i\n", transform_number);
+  // printf("transform_number\n");
+  return 0;
+}
+
+// g++ -Wall -std=c++17 program_022.cpp -o output
+// ./output
