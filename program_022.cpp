a703a08eae19846eb449b439ec9afa2c32c49263
Anirudh Jayaraman <anirudhjay@gmail.com>
2021-06-27 21:27:04 +0530

    Program 22: Coding Exercise 8 with Increment and Decrement Operators

--- program_022.cpp
+++ program_022.cpp
@@ -1,5 +1,6 @@
 /*
 Coding Exercise 8
+with Increment / Decrement operators
 */
 
 #include <iostream>
@@ -13,15 +14,15 @@ int arithmetic_operators(int number) {
     number = number * 2;
 
     //-- Add 9 to number and assign the result back to number
-    number = number + 9;
+    number += 9;
     //-- Subtract 3 from number and assign the result back to number
-    number = number - 3;
+    number -= 3;
     //-- Divide number by 2 and assign the result back to number
-    number = number / 2;
+    number /= 2;
     //-- Subtract original_number from number and assign the result back to number
-    number = number - original_number;
+    number -= original_number;
     //-- Take the modulus 3 (%) of number and assign it back to number
-    number = number % 3;
+    number %= 3;
 
     //----WRITE YOUR CODE ABOVE THIS LINE----
 
